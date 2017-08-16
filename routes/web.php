bba38cd61dc649146cb1276b01e005892dc444b6
ashan <ashanrupasinghe11@gmail.com>
2017-08-16 19:40:20 +0530

    admin user pw reset works fine

--- routes/web.php
+++ routes/web.php
@@ -25,5 +25,10 @@ Route::prefix('admin')->group(function(){
 	Route::get('/logout', 'Auth\AdminLoginController@logout')->name('admin.logout');
 	Route::get('/', 'AdminController@index')->name('admin.dashboard');
 	
+	Route::post('/password/email', 'Auth\AdminForgotPasswordController@sendResetLinkEmail')->name('admin.password.email');
+	Route::get('/password/reset', 'Auth\AdminForgotPasswordController@showLinkRequestForm')->name('admin.password.request');
+	Route::post('/password/reset', 'Auth\AdminResetPasswordController@reset')->name('admin.dashboard');
+	Route::get('/password/reset/{token}', 'Auth\AdminResetPasswordController@showResetForm')->name('admin.password.reset');
+	
 });
 
